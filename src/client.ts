4ac1829be0aa1618ec5a246e7417bf379b2f2f28
Ed Adjei <edward.adjei@aragorn.ai>
2025-07-13 21:28:57 -0500

    Adding support for TLS

--- src/client.ts
+++ src/client.ts
@@ -5,12 +5,15 @@ const options = {
   username: process.env.CLICKHOUSE_USER,
   password: process.env.CLICKHOUSE_PASSWORD || '',
   database: process.env.CLICKHOUSE_DB,
-  port: process.env.CLICKHOUSE_PORT || '8123',
+  port: process.env.CLICKHOUSE_PORT,
+  useTls: process.env.CLICKHOUSE_USE_TLS === 'true',
 };
 
+const protocol = options.useTls ? 'https' : 'http';
+
 export const clickhouse = createClient({
-  url: `http://${options.username}:${encodeURIComponent(options.password)}@${
-    options.host
-  }:${options.port}`,
+  url: `${protocol}://${options.username}:${encodeURIComponent(
+    options.password,
+  )}@${options.host}:${options.port}`,
   database: options.database,
 });
